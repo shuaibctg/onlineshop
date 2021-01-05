df67ae5a6a622462e6ba38459ab1eb2ab8f84624
shuaibctg <64361508+shuaibctg@users.noreply.github.com>
2021-01-05 08:58:48 +0600

    Update onlineshop-index.py

--- onlineshop-index.py
+++ onlineshop-index.py
@@ -1,50 +1,112 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Product
-from math import ceil
 
 
-# Create your views here.
-def index(request):
-    #products = Product.objects.all()
-    #n = len(products)
-    #nSlides = n//4 + ceil((n/4)-(n//4))
-    allProds = []
-    catprods = Product.objects.values('category', 'id')
-    cats = {item['category'] for item in catprods }
-    for cat in cats:
-        prod = Product.objects.filter(category=cat)
-        n = len(prod)
-        nSlides = n // 4 + ceil((n / 4) - (n // 4))
-        allProds.append([prod, range(1, nSlides), nSlides])
+
+{% extends "shop/basic.html"%}
+
+{%block css%}
+      .col-md-3{
+      display: inline-block;
+      margin-left: -4px;
+      }
+      .col-md-3 img{
+      width:250px;
+      height: 250px;
+      }
+      body .carousel-indicator li{
+      background-color:blue;
+      }
+      body .carousel-control-prev-icon,
+      body .carousel-control-next-icon{
+      background-color:blue;
+      }
+        .carousel-control-prev,
+        .carousel-control-next{
+        top: auto;
+        bottom: auto;
+        }
+      body .no-padding{
+      padding-left:0,
+      padding-right:0;
+      }
+{%endblock%}
+
+{%block body%}
+  {%load static %}
+<div class="container">
+              <!-- SlideShow Starts Here! -->
+    {% for product, range, nSlides in allProds %}
+    <h5>Flash Sales! {{product.0.category}} - Recommended Items</h5>
+    <div id="demo{{forloop.counter}}" class="carousel slide my-3" data-ride="carousel">
+      <ul class="carousel-indicators">
+        <li data-target="#demo{{forloop.counter}}" data-slide-to="0" class="active"></li>
+
+          {% for i in range%}
+        <li data-target="#demo{{forloop.parantloop.counter}}" data-slide-to="{{i}}"></li>
+          {% endfor %}
+      </ul>
 
 
-    # params = {'no_of_slides': nSlides, 'range': range(1, nSlides), 'product': products}
-    #allProds = [[products, range(1, nSlides), nSlides],
-               # [products, range(1, nSlides), nSlides]]
-    params = {'allProds': allProds}
-    return render(request, 'shop/index.html', params)
+        <div class="container carousel-inner no-padding">
+          <div class="carousel-item active">
+            <div class="col-xs-3 col-sm-3 col-md-3">
+              <div class="card" style="width: 18rem;">
+                <img src='/media/{{product.0.image}}' class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{product.0.product_name}}</h5>
+                    <p class="card-text">{{product.0.desc}}</p>
+                    <button class="btn btn-primary cart">Add To Cart</button>
+                </div>
+              </div>
+            </div>
+
+              {% for i in product|slice:"1:" %}
+            <div class="col-xs-3 col-sm-3 col-md-3">
+              <div class="card" style="width: 18rem;">
+                <img src='/media/{{i.image}}' class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{i.product_name}}</h5>
+                    <p class="card-text">{{i.desc}}</p>
+                    <button class="btn btn-primary cart">Add To Cart</button>
+                </div>
+              </div>
+            </div>
+                {% if forloop.counter|divisibleby:3 and forloop.counter > 0 and not forloop.last %}
+            </div><div class="carousel-item">
+                {% endif %}
+              {% endfor %}
+            </div>
+
+          </div>
+
+    </div>
 
 
-def about(request):
-    return render(request, "shop/about.html")
+<!-- left right control -->
+    <a class="carousel-control-prev" href="#demo{{forloop.counter}}" data-slide="prev">
+      <span class="carousel-control-prev-icon"></span>
+    </a>
 
+    <a class="carousel-control-next" href="#demo{{forloop.counter}}" data-slide="next">
+      <span class="carousel-control-next-icon"></span>
+    </a>
+{% endfor %}
+</div>
+{%endblock%}
 
-def contact(request):
-    return HttpResponse("We are at contact Page")
+{% block js%}
+<script>
+if(localStorage.getItem('cart') == null){
+   var cart = {};
+}
+else{
+cart = JSON.parse(localStorage.getItem('cart'));
+}
 
+$('.cart').click(function(){
+console.log('clicked');
+});
 
-def tracker(request):
-    return HttpResponse("We are at tracker Page")
+    console.log("working");
+</script>
 
-
-def search(request):
-    return HttpResponse("We are at search Page")
-
-
-def productView(request):
-    return HttpResponse("We are at productVies Page")
-
-
-def checkout(request):
-    return HttpResponse("We are at checkout Page")
+{% endblock %}
